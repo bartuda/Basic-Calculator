ee3146a07d41f5c2e902ad8bcced2a18d1063605
Dan <bartuda@gmail.com>
2021-01-24 07:25:22 +0000

    Initial commit

---
+++ Building a basic calculator.py
@@ -0,0 +1,12 @@
+num1 = input("Enter a whole number: ")
+num2 = input("Enter another whole number: ")
+result = int(num1) + int(num2)   #int convert strings into whole numbers (no decimals)
+
+print(result)
+
+num3 = input("Enter a  number: ")
+num4 = input("Enter another  number: ")
+
+result1 = float(num3) + float(num4) #float convert strings into numbers
+
+print(result1)
